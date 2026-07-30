5aed3c7b589d689392def35814c7c4ce02cc4e38
ColdWaterStudio <ColdColaOfficial@outlook.com>
2026-07-30 21:48:09 +0800

    Update AbstractMinecartMixin.java

--- src/main/java/expresscarts/mixin/AbstractMinecartMixin.java
+++ src/main/java/expresscarts/mixin/AbstractMinecartMixin.java
@@ -1,24 +1,39 @@
 package expresscarts.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import expresscarts.ExpressMinecartEntity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.vehicle.minecart.AbstractMinecart;
-import net.minecraft.world.entity.vehicle.VehicleEntity;
-import net.minecraft.world.level.Level;
+import expresscarts.ExpressCartsConfig;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Mixin(AbstractMinecartEntity.class)
+public abstract class AbstractMinecartMixin {
 
-@Mixin(AbstractMinecart.class)
-public abstract class AbstractMinecartMixin extends VehicleEntity {
-    public AbstractMinecartMixin(EntityType<?> entityType, Level level) {
-        super(entityType, level);
+    @Inject(method = "getMaxSpeed", at = @At("HEAD"), cancellable = true)
+    private void modifyMaxSpeed(CallbackInfoReturnable<Double> cir) {
+        cir.setReturnValue(ExpressCartsConfig.maxCartSpeed);
     }
 
-    @ModifyExpressionValue(method = "*", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/vehicle/minecart/AbstractMinecart;useExperimentalMovement(Lnet/minecraft/world/level/Level;)Z"))
-    private boolean useExperimentalBehaviorForExpressMinecart(boolean original) {
-        return ((AbstractMinecart) (Object) this) instanceof ExpressMinecartEntity || original;
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void applyAcceleration(CallbackInfo ci) {
+        AbstractMinecartEntity cart = (AbstractMinecartEntity) (Object) this;
+        if (!cart.getWorld().isClient() && cart.hasPassengers()) {
+            Entity passenger = cart.getFirstPassenger();
+            if (passenger instanceof PlayerEntity player) {
+                if (player.forwardSpeed > 0) {
+                    Vec3d vel = cart.getVelocity();
+                    cart.setVelocity(
+                        vel.x * ExpressCartsConfig.accelerationMultiplier,
+                        vel.y,
+                        vel.z * ExpressCartsConfig.accelerationMultiplier
+                    );
+                }
+            }
+        }
     }
-
 }
